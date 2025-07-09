69e3842cec59c836df91ba5471a8e04ddaa19386
kushal singh <kushalsingh10b@gmail.com>
2025-07-09 21:59:26 +0530

    changed the mongobd

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 //connecting to mongoose
 export const connectDB= async()=>{
     try {
-        const con=await mongoose.connect('mongodb://127.0.0.1:27017/devchat');
+        const con=await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${con.connection.host}`);
     } catch (error) {
         console.log("error connecting to mongoDB");
